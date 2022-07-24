cd217d2dddff840bb9482072493252232a5ad401
21f2000346 <109918437+21f2000346@users.noreply.github.com>
2022-07-25 00:17:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ import pandas as pd
 #Get Input
  
 st.title('ODD OR EVEN')
-st.header('Check Number is Odd or Even')
-x = st.number_input("GET_NUMBER",min_value=0,step=1)
+st.header('Check the Number is Odd or Even')
+x = st.number_input("Please Enter a Natural Number",min_value=0,step=1)
 if(st.button("Check ODD or EVEN")):
     if (x%2 ==0) :
         st.subheader("You have Entered an EVEN number")
